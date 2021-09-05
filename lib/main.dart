6f0beb6f4aafbd4579fb8d091fb2e988e9f255c5
azkaBashir45 <azka.ngxoft@gmail.com>
2021-09-05 08:34:03 -0700

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,17 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputPage(),
-      
-    );
+        home: InputPage(),
+        //theme change
+        theme: ThemeData(
+          primaryColor: Colors.black,
+          scaffoldBackgroundColor: Colors.black,
+          textTheme: const TextTheme(bodyText2: TextStyle(color: Colors.white)),
+        ));
+    // theme: ThemeData().copyWith(
+    //   primaryColor: Colors.black,
+    //   scaffoldBackgroundColor: Colors.black,
+    // ));
   }
 }
 
@@ -26,9 +34,6 @@ class _InputPageState extends State<InputPage> {
       ),
       body: Center(
         child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
       ),
     );
   }
