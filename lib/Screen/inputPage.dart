6771f5e530261298156c9067cfd6c1ab461af607
azkaBashir45 <azka.ngxoft@gmail.com>
2021-09-14 12:43:29 -0700

    enum

--- lib/Screen/inputPage.dart
+++ lib/Screen/inputPage.dart
@@ -3,12 +3,37 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'Container.dart';
 import 'iconText.dart';
 
+const Color activeColor = Color(0xFF1D1E33);
+const Color deactiveColor = Color(0xFF111328);
+enum Gender { male, female }
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleColor = deactiveColor;
+  Color femaleColor = deactiveColor;
+//without enum
+  // void updatetheColor(int gender) {
+  //   if (gender == 1) {
+  //     maleColor = activeColor;
+  //   }
+  //   if (gender == 2) {
+  //     maleColor = activeColor;
+  //   }
+  // }
+//with enum integer ki form m yd rkna muskil hojata h hm text ki form m yd rky gy
+  void updatetheColor(Gender genderType) {
+    if (genderType == Gender.male) {
+      maleColor = activeColor;
+    }
+    if (genderType == Gender.female) {
+      maleColor = activeColor;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,21 +46,33 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: RepeateContainer(
-                    colors: Color(0xFF1D1E33),
-                    cardWidjet: RepeatTextIcon(
-                      iconData: FontAwesomeIcons.male,
-                      label: 'Male',
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updatetheColor(Gender.male);
+                      });
+                    },
+                    child: RepeateContainer(
+                      colors: maleColor,
+                      cardWidjet: RepeatTextIcon(
+                        iconData: FontAwesomeIcons.male,
+                        label: 'Male',
+                      ),
                     ),
                   ),
                 ),
                 Expanded(
-                  child: RepeateContainer(
-                    colors: Color(
-                      0xFF1D1E33,
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updatetheColor(Gender.female);
+                      });
+                    },
+                    child: RepeateContainer(
+                      colors: femaleColor,
+                      cardWidjet: RepeatTextIcon(
+                          iconData: FontAwesomeIcons.female, label: 'Female'),
                     ),
-                    cardWidjet: RepeatTextIcon(
-                        iconData: FontAwesomeIcons.female, label: 'Female'),
                   ),
                 ),
               ],
